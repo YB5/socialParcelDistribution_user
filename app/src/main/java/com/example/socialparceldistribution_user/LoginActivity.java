af7ac92cd11b8fdb8cdd8e6ee233fba9fda01dbb
ShemuelUriyaAschkenazy <uriyaas@gmail.com>
2020-01-30 03:46:30 +0200

    login

---
+++ app/src/main/java/com/example/socialparceldistribution_user/LoginActivity.java
@@ -0,0 +1,140 @@
+package com.example.socialparceldistribution_user;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private EditText fullNameEt;
+    private EditText passwordEt;
+    private EditText emailEt;
+    private Button signIn_bt;
+    private Button signUp_bt;
+    private String fullName;
+    private String userName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        fullNameEt = findViewById(R.id.et_full_name);
+        passwordEt = findViewById(R.id.et_password);
+        emailEt = findViewById(R.id.et_email);
+        signIn_bt = findViewById(R.id.bt_sign_in);
+        signUp_bt = findViewById(R.id.bt_sign_up);
+        final ConstraintLayout constraintLayout = findViewById(R.id.container);
+
+
+        signUp_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!check_validate()) {
+                    Toast.makeText(LoginActivity.this, "input is not valid, try again", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                fullName = fullNameEt.getText().toString();
+                String password = passwordEt.getText().toString();
+                String email = emailEt.getText().toString();
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                           // Toast.makeText(LoginActivity.this,"successful, welcome!!!",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra("username",fullName);
+                            startActivity(intent);
+                        } else {
+                            Snackbar.make(constraintLayout, "unsuccess to sign-up", Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        signIn_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!check_validate()) {
+                    Toast.makeText(LoginActivity.this, "input is not valid, try again", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                String password = passwordEt.getText().toString();
+                String email = emailEt.getText().toString();
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra("username",firebaseAuth.getCurrentUser().getDisplayName());
+                            startActivity(intent);
+                        } else {
+                            Snackbar.make(constraintLayout, "unsuccess to sign-in", Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                // TextView userTv = findViewById(R.id.user_tv);
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                userName=user.getDisplayName();
+                if (user != null) {//sign up or sign in
+                    if (fullName != null && !fullName.isEmpty()) { //sign up - update profile with full name
+
+                        user.updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(fullName).build()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                fullName = null;
+                                if (task.isSuccessful())
+                                   Toast.makeText(LoginActivity.this,user.getDisplayName()+ " logged in. Welcome!!",Toast.LENGTH_LONG).show();
+                                 //Snackbar.make(linearLayout, user.getDisplayName() + " Welcome!!!", Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    //userTv.setText(user.getDisplayName() + " logged in");
+                } //else {
+                //userTv.setText("please log in");
+
+                //}
+            }
+        };
+    }
+
+    private boolean check_validate() {
+        if (fullNameEt.getText().toString().isEmpty() || emailEt.getText().toString().isEmpty() || passwordEt.getText().toString().isEmpty())
+            return false;
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+}
+
--- app/src/main/java/com/example/socialparceldistribution_user/MainActivity.java
+++ app/src/main/java/com/example/socialparceldistribution_user/MainActivity.java
@@ -1,121 +1,79 @@
 package com.example.socialparceldistribution_user;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.HeaderViewListAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener authStateListener;
-    private EditText fullNameEt;
-    private EditText passwordEt;
-    private EditText emailEt;
-    private Button signIn_bt;
-    private Button signOut_bt;
-    private Button signUp_bt;
-    private String fullName;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        fullNameEt = findViewById(R.id.et_full_name);
-        passwordEt = findViewById(R.id.et_password);
-        emailEt = findViewById(R.id.et_email);
-        signIn_bt = findViewById(R.id.bt_sign_in);
-        signOut_bt = findViewById(R.id.bt_sign_out);
-        signUp_bt = findViewById(R.id.bt_sign_up);
-        final LinearLayout linearLayout = findViewById(R.id.linearLayout);
-
-        signUp_bt.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                fullName = fullNameEt.getText().toString();
-                String password = passwordEt.getText().toString();
-                String email = emailEt.getText().toString();
-                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Snackbar.make(linearLayout, "success sign-up", Snackbar.LENGTH_LONG).show();
-                        } else {
-                            Snackbar.make(linearLayout, "unsuccess to sign-up", Snackbar.LENGTH_LONG).show();
-                        }
-                    }
-                });
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
-        signIn_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String password = passwordEt.getText().toString();
-                String email = emailEt.getText().toString();
-                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Snackbar.make(linearLayout, "Wwlcome", Snackbar.LENGTH_LONG).show();
-                        } else {
-                            Snackbar.make(linearLayout, "unsuccess to sign-in", Snackbar.LENGTH_LONG).show();
+        View nav_header= navigationView.getHeaderView(0);
+        TextView username = nav_header.findViewById(R.id.sign_in_as_tv);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null)
+            username.setText("connected as: "+extras.getString("username"));
 
-                        }
-                    }
-                });
-            }
-        });
-
-        authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                TextView userTv = findViewById(R.id.user_tv);
-                final FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {//sign up or sign in
-                    if (fullName!=null&&!fullName.isEmpty()) { //sign up - update profile with full name
-
-                        user.updateProfile(new UserProfileChangeRequest.Builder().setDisplayName(fullName).build()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                fullName = null;
-                                if (task.isSuccessful())
-                                    Snackbar.make(linearLayout, user.getDisplayName() + " Welcome!!!", Snackbar.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                    userTv.setText(user.getDisplayName() + " logged in");
-                } else {
-                    userTv.setText("please log in");
-
-                }
-            }
-        };
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        firebaseAuth.addAuthStateListener(authStateListener);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
-

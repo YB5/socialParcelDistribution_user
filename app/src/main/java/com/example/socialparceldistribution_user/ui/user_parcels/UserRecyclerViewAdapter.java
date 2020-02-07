8c881fdc7aaa263a7f80ff242271fae0c03826ee
ShemuelUriyaAschkenazy <uriyaas@gmail.com>
2020-02-07 12:41:35 +0200

    עיצוב של פparcelcell

--- app/src/main/java/com/example/socialparceldistribution_user/ui/user_parcels/UserRecyclerViewAdapter.java
+++ app/src/main/java/com/example/socialparceldistribution_user/ui/user_parcels/UserRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.socialparceldistribution_user.ui.user_parcels;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
 
     private List<Parcel> parcels;
     private SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+
     public UserRecyclerViewAdapter(List<Parcel> parcels) {
         this.parcels = parcels;
     }
@@ -30,12 +32,12 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
         void onButtonClicked(int position, View view);
     }
 
-    void setListener(MyParcelsListener listener){
-        this.listener=listener;
+    void setListener(MyParcelsListener listener) {
+        this.listener = listener;
     }
 
 
-    class HistoryParcelViewHolder extends RecyclerView.ViewHolder{
+    class HistoryParcelViewHolder extends RecyclerView.ViewHolder {
 
         TextView status;
         TextView recipientName;
@@ -45,41 +47,43 @@ public class UserRecyclerViewAdapter extends RecyclerView.Adapter<UserRecyclerVi
         TextView date;
         TextView messengerName;
         Button seeSuggestions;
-Button setArrivedParcel;
+        Button setArrivedParcel;
+
         private HistoryParcelViewHolder(View itemView) {
             super(itemView);
 
-            status=itemView.findViewById(R.id.status_tv);
-            recipientName=itemView.findViewById(R.id.recipient_name_tv);
-            parcelType=itemView.findViewById(R.id.type_tv);
-            recipientAddress=itemView.findViewById(R.id.recipientAddressTv);
-            warehouseAddress =itemView.findViewById(R.id.warehouseAddressTv);
-            date=itemView.findViewById(R.id.date_tv);
-            messengerName=itemView.findViewById(R.id.messenger_name_tv);
-            seeSuggestions=itemView.findViewById(R.id.bt_see_suggestions);
+            status = itemView.findViewById(R.id.status_tv);
+            recipientName = itemView.findViewById(R.id.recipient_name_tv);
+            parcelType = itemView.findViewById(R.id.type_tv);
+            recipientAddress = itemView.findViewById(R.id.recipientAddressTv);
+            warehouseAddress = itemView.findViewById(R.id.warehouseAddressTv);
+            date = itemView.findViewById(R.id.date_tv);
+            messengerName = itemView.findViewById(R.id.messenger_name_tv);
+            seeSuggestions = itemView.findViewById(R.id.bt_see_suggestions);
             seeSuggestions.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
-                    if (listener!=null)
-                        listener.onButtonClicked(getAdapterPosition(),view);
+                    if (listener != null)
+                        listener.onButtonClicked(getAdapterPosition(), view);
                 }
             });
-            setArrivedParcel= itemView.findViewById(R.id.bt_arrivedParcel);
+            setArrivedParcel = itemView.findViewById(R.id.bt_arrivedParcel);
             setArrivedParcel.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (listener!=null)
-                        listener.onButtonClicked(getAdapterPosition(),view);
+                    if (listener != null)
+                        listener.onButtonClicked(getAdapterPosition(), view);
                 }
             });
 
         }
     }
+
     @NonNull
     @Override
     public HistoryParcelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.parcel_cell, parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.parcel_cell, parent, false);
         return new HistoryParcelViewHolder(view);
     }
 
@@ -87,13 +91,15 @@ Button setArrivedParcel;
     public void onBindViewHolder(@NonNull HistoryParcelViewHolder holder, int position) {
         Parcel parcel = parcels.get(position);
         //holder.messengerName.setText(parcel.getMessengers().isEmpty()?"no messenger": parcel.getMessengerName()+"");
-        holder.date.setText(parcel.getDeliveryDate()==null? "no date":format.format(parcel.getDeliveryDate()));
-        holder.recipientAddress.setText(parcel.getRecipientAddress().isEmpty()?"no address":parcel.getRecipientAddress());
-        holder.recipientName.setText(parcel.getRecipientName().isEmpty()?"no recipient name":parcel.getRecipientName());
-        holder.parcelType.setText(parcel.getParcelType()==null?"no type":parcel.getParcelType().toString());
-        holder.status.setText(parcel.getParcelStatus()==null?"no status":parcel.getParcelStatus().toString());
-        holder.seeSuggestions.setEnabled(parcel.getMessengers()!=null?true:false);
-        holder.seeSuggestions.setText(parcel.getMessengers()!=null?"see your "+parcel.getMessengers().size()+" suggestions":"no suggestions");
+        holder.date.setText(parcel.getDeliveryDate() == null ? "no date" : format.format(parcel.getDeliveryDate()));
+        holder.recipientAddress.setText(parcel.getRecipientAddress().isEmpty() ? "no recipient address" : parcel.getRecipientAddress());
+        holder.warehouseAddress.setText(parcel.getWarehouseAddress().isEmpty() ? "no warehouse address" : parcel.getRecipientAddress());
+        holder.recipientName.setText(parcel.getRecipientName().isEmpty() ? "no recipient name" : parcel.getRecipientName());
+        holder.parcelType.setText(parcel.getParcelType() == null ? "no type" : parcel.getParcelType().toString());
+        holder.status.setText(parcel.getParcelStatus() == null ? "no status" : parcel.getParcelStatus().toString());
+        holder.seeSuggestions.setEnabled(parcel.getMessengers() != null ? true : false);
+        holder.seeSuggestions.setText(parcel.getMessengers() != null ? parcel.getMessengers().size() + " suggestions" : "no suggestions");
+        holder.seeSuggestions.setBackgroundColor(parcel.getMessengers() != null && parcel.getMessengers().size() > 0 ? Color.GREEN : Color.GRAY);
     }
 
     @Override
